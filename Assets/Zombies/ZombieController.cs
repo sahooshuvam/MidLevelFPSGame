08d580d20dbc188984e0c1eeb435eb5ffefe7ffd
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-04-11 18:59:22 +0530

    Added GameScene....

---
+++ Assets/Zombies/ZombieController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    Animator anim;
+    public GameObject target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        anim.SetBool("isWalking", true);
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isAttacking", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", true);
+        }
+        /*
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+            anim.SetBool("isWalking", false);
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+            anim.SetBool("isRunning", false);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+            anim.SetBool("isAttacking", false);
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            anim.SetBool("isDead", true);
+        }*/
+
+    }
+}
